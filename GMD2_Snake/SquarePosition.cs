fe84ac36c9ea950f8324dfe04965e8431c1cd7e2
Avalin <maoraw@gmail.com>
2020-04-01 14:53:11 +0200

    SquarePosition Class

--- GMD2_Snake/SquarePosition.cs
+++ GMD2_Snake/SquarePosition.cs
@@ -2,11 +2,33 @@
 {
     public class SquarePosition
     {
+        int x;
+        int y;
+        int width;
+        int height;
+
         public SquarePosition(int x, int y, int width, int height)
         {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
 
+        public int[] GetPosition() 
+        {
+            int[] position = { x, y };
+            return position;
+        }
 
+        public int GetWidth() 
+        {
+            return width;
+        }
 
+        public int GetHeight()
+        {
+            return height;
         }
     }
 }
